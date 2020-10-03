68927ffa113e27216f4b5a1c6d1ae5177dd3111c
adripiki4 <adriba@alu.iesserpis.org>
2020-10-03 20:06:23 +0200

    Creando holamundo 3 con boton de volver

---
+++ AndroidStudioProjects/Holamundo3/app/src/main/java/com/example/holamundo3/Pantalla2.java
@@ -0,0 +1,71 @@
+package com.example.holamundo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Pantalla2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+
+        final TextView otroSaludo= (TextView)findViewById(R.id.miMensaje);
+        final Button volverBtn= (Button)findViewById(R.id.miVolver);
+        final ImageButton imgBtn= (ImageButton)findViewById(R.id.imgVolver);
+
+        Bundle  miBundleRecoger = getIntent().getExtras();
+        otroSaludo.setText(miBundleRecoger.getString("TEXTO"));
+        final String completarSaludo=miBundleRecoger.getString("TEXTO");
+        volverBtn.setOnClickListener( new View.OnClickListener(){
+            public void onClick(View v){
+                Intent vueltaIntent= new Intent();
+                Bundle vueltaBundle=new Bundle();
+                String mensajeDevuelto= "Devuelvo a Principal " + completarSaludo;
+                vueltaBundle.putString("DEVUELTO", mensajeDevuelto);
+                vueltaIntent.putExtras(vueltaBundle);
+                setResult(RESULT_OK, vueltaIntent);
+                finish();
+
+            }
+        });
+
+    }
+    /* Proceso para entender el ciclo de vida */
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart-Pantalla2", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume-Pantalla2", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onPause() {
+        Toast.makeText(this, "onPause-Pantalla2", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop-Pantalla2", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"onRestart-Pantalla2", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this,"onDestroy", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+}
