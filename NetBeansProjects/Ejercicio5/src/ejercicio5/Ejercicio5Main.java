51147ab6b9dc84b38aa607eb9724678e8590cf5e
adripiki4 <adriba@alu.iesserpis.org>
2020-10-06 12:40:03 +0200

    subiendo ejercicio5

--- NetBeansProjects/Ejercicio5/src/ejercicio5/Ejercicio5Main.java
+++ NetBeansProjects/Ejercicio5/src/ejercicio5/Ejercicio5Main.java
@@ -5,6 +5,8 @@
  */
 package ejercicio5;
 
+import java.util.Random;
+
 /**
  *
  * @author adriba
@@ -15,7 +17,58 @@ public class Ejercicio5Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int vector[] = new int[15];
+        int matriz[][] = new int [5][3];
+        int aleatorio;
+        boolean rellena= false;
+        boolean valor = true;
+        
+        Random random = new Random();
+        
+        
+        
+        for (int i = 0; i <15; i++) {
+            rellena=false;
+            while(rellena==false){
+            aleatorio = random.nextInt(90)+1;
+            valor = Comprueba_numero(vector, aleatorio);
+            if(valor==true){
+                vector[i]=aleatorio;
+                rellena = true;
+            }else if (valor==false){
+                rellena=false;
+            }
+            }   
+            
+        }
+        
+        for (int x: vector) {
+            System.out.println(x);
+        }
+        
+        for (int i = 0; i <15 ; i++) {
+            for (int j = 0; j < 15; j++) {
+                
+                
+            }
+        }
+        
+         
+    }
+    
+    public static boolean Comprueba_numero(int vector[],int aleatorio){
+       int comprueba;
+       boolean valor = true;
+       
+       for(int i = 0; i<15;i++){
+           comprueba = vector[i];
+           if(comprueba == aleatorio){
+               valor = false;
+               break;
+           }
+       }
+        return valor;
+        
     }
     
 }
