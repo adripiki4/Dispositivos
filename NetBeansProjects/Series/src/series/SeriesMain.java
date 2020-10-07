c6ab1c973e2c78d8f329265f2073c9b9118ff76f
adripiki4 <adripiki4@gmail.com>
2020-10-07 11:48:00 +0200

    subiendo frame desde casa

--- NetBeansProjects/Series/src/series/SeriesMain.java
+++ NetBeansProjects/Series/src/series/SeriesMain.java
@@ -8,6 +8,7 @@ package series;
 import model.access;
 import model.listshow;
 import model.show;
+import view.Frame;
 
 /**
  *
@@ -19,35 +20,37 @@ public class SeriesMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-//        show sh = new show("Breaking Bad","Vicent Guilligan",5,"Drama",5);
-        listshow ls = new listshow();
-//        ls.setshow(sh);
-        
-////        sh=new show("JGame of thrones","David Benioff", 6, "1", 6);
-//        ls.setshow(sh);
+//        // TODO code application logic here
+////        show sh = new show("Breaking Bad","Vicent Guilligan",5,"Drama",5);
+//        listshow ls = new listshow();
+////        ls.setshow(sh);
 //        
-//        access.saveLS(ls);
+//////        sh=new show("JGame of thrones","David Benioff", 6, "1", 6);
+////        ls.setshow(sh);
+////        
+////        access.saveLS(ls);
+////        
+////        System.out.println("saved series");
+//
+////REad  the file
+//        ls=access.loadLS();
+////        Create a show
+//        show sh = new show();
+////        obtain firts element of arraylist:
+//        sh=ls.getShow(0);
+////      get the tittle
+//        System.out.print("Tittle: ");
+//        System.out.println(sh.getTitle());
 //        
-//        System.out.println("saved series");
-
-//REad  the file
-        ls=access.loadLS();
-//        Create a show
-        show sh = new show();
-//        obtain firts element of arraylist:
-        sh=ls.getShow(0);
-//      get the tittle
-        System.out.print("Tittle: ");
-        System.out.println(sh.getTitle());
-        
-//        obatin the second element:
-        sh =ls.getShow(1);
-//      get the title2
-        System.out.print("Tittle: ");
-        System.out.println(sh.getTitle());
-        
-        
+////        obatin the second element:
+//        sh =ls.getShow(1);
+////      get the title2
+//        System.out.print("Tittle: ");
+//        System.out.println(sh.getTitle());
+//        
+//        
+        Frame f = new Frame();
+        f.setVisible(true);
         
         
     }
--- NetBeansProjects/Series/src/view/Frame.java
+++ NetBeansProjects/Series/src/view/Frame.java
@@ -5,6 +5,11 @@
  */
 package view;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,7 +23,7 @@ import javax.swing.JTextField;
 public class Frame extends JFrame {
     private JPanel panel1 = new JPanel();
     private JPanel panel2 = new JPanel();
-    private JPanel panle3 = new JPanel();
+    private JPanel panel3 = new JPanel();
     private JButton b1,b2,b3,b4,b5,b6,b7;
     private JLabel e1,e2,e3,e4,e5,e6;
     private JTextField t1,t2,t3,t4,t5,t6;
@@ -28,7 +33,59 @@ public class Frame extends JFrame {
         setTitle("My series");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         panel1.setLayout(new BorderLayout());
+        panel2.setLayout(new GridLayout(7,2,10,10));
+        panel3.setLayout(new FlowLayout());
+        add(panel1);
+        panel1.add(panel3,BorderLayout.NORTH);
+        panel1.add(panel2,BorderLayout.CENTER);
+        b1= new JButton("|<");
+        b2 = new JButton("<");
+        b3 = new JButton(">");
+        b4 = new JButton("<|");
+        b5 = new JButton("+");
+        b6 = new JButton("-");
+        b7 = new JButton("*");
+        panel3.add(b1);
+        panel3.add(b2);
+        panel3.add(b3);
+        panel3.add(b4);
+        panel3.add(b5);
+        panel3.add(b6);
+        panel3.add(b7);
+        e1 = new JLabel("Title");
+        t1 = new JTextField(50);
+        panel2.add(e1);
+        panel2.add(t1);
+        e2 = new JLabel("Screenwriter");
+        t2 = new JTextField(50);
+        panel2.add(e2);
+        panel2.add(t2);
+        e3 = new JLabel("Season");
+        t3 = new JTextField(5);
+        panel2.add(e3);
+        panel2.add(t3);
+        e4 = new JLabel("Genre");
+        t4 = new JTextField(50);
+        panel2.add(e4);
+        panel2.add(t4);
+        e5 = new JLabel("Seen seasons");
+        t5 = new JTextField(5);
+        panel2.add(e5);
+        panel2.add(t5);
+        t1.setEditable(false);
+        t2.setEditable(false);
+        t3.setEditable(false);
+        t4.setEditable(false);
+        t5.setEditable(false);
+        t6.setEditable(false);
         
     }
     
+    
+    
 }
+//class buttonsListener implements ActionListener{
+//        public void actionPerformed(ActionEvent e) {
+//        }
+//        
+//    }
