4988181fd6da32b1c6e4e44a2c207fefadd45f3d
adripiki4 <adriba@alu.iesserpis.org>
2020-10-09 14:22:17 +0200

    Creando la clase persona

--- NetBeansProjects/Herencia2/src/herencia2/Persona.java
+++ NetBeansProjects/Herencia2/src/herencia2/Persona.java
@@ -5,6 +5,8 @@
  */
 package herencia2;
 
+import java.util.Random;
+
 /**
  *
  * @author adriba
@@ -13,33 +15,35 @@ public class Persona {
     private String nombre;
     private int edad;
     private String dni;
-    private String sexo;
+    private char sexo;
     private double peso;
     private double altura;
+    private Random random;
     
     public Persona(){
         this.nombre = "";
         this.edad = 0;
-        this.dni = "";
-        this.sexo = "hombre";
+        this.dni = generaDni();
+        this.sexo = 'H';
         this.peso = 0;
         this.altura = 0;
     }
     
-    public Persona(String nombre, int edad, String sexo){
+    public Persona(String nombre, int edad, char sexo){
         this.nombre = nombre;
         this.edad = edad;
-        this.dni = "";
-        this.sexo = sexo;
+        this.dni = generaDni();
+        this.sexo = compruebaSexo(sexo);
         this.peso = 0;
         this.altura = 0;
+        
     }
     
-    public Persona(String nombre, int edad, String dni, String sexo, double peso, double altura){
+    public Persona(String nombre, int edad, String dni, char sexo, double peso, double altura){
         this.nombre = nombre;
         this.edad = edad;
         this.dni = dni;
-        this.sexo = sexo;
+        this.sexo = compruebaSexo(sexo);
         this.peso = peso;
         this.altura = altura;
     }
@@ -66,10 +70,54 @@ public class Persona {
         return res;
     }
     
+    public char compruebaSexo(char sexo){
+        if(sexo!='H' && sexo!='M'){
+            sexo = 'H';
+        }else{
+            sexo = sexo;
+        }
+            return sexo;
+    }
+    
     public String toString(){
         String res = "";
+        res = "Nombre: "+nombre+", Edad: "+edad+", DNI: "+dni+", Sexo: "+sexo+", Peso: "+peso+", Altura: "+altura;
+        return res;
+    }
+    public String generaDni(){
+        String res ="";
+        String letra;
+        String letras[]={"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+        int numero;
+        int resto;
+        random = new Random();
+        
+        numero = random.nextInt(99999999)+1;
+        resto = numero % 23;
+        letra = letras[resto];
+        res = Integer.toString(numero)+" "+letra;
         
         return res;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
     }
     
 }
