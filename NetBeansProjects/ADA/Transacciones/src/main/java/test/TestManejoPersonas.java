d5d3a7a1a6b4947d71727be858b0f42220015ccd
adripiki4 <adriba@alu.iesserpis.org>
2020-11-09 12:12:28 +0100

    empezando transacciones

---
+++ NetBeansProjects/ADA/Transacciones/src/main/java/test/TestManejoPersonas.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import datos.Conexion;
+import static datos.Conexion.close;
+import datos.PersonaDAO;
+import domain.Persona;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author adriba
+ */
+public class TestManejoPersonas {
+
+
+    public static void main(String[] args) throws SQLException {
+        PersonaDAO personaDao = new PersonaDAO();
+        List<Persona> personas = personaDao.seleccionar();
+        
+
+        //insertando neuvo objeto de tipo persona
+        Persona personaNueva = new Persona("Fidel", "Hernadez", 47);
+        personaDao.insertar(personaNueva);
+        for (Persona persona : personas) {
+            System.out.println("persona = " + persona);
+        }
+        
+
+    }
+
+
+
+}
---
+++ NetBeansProjects/ADA/Transacciones/src/main/java/test/TestMysqlJDBC.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.sql.*;
+
+
+
+/**
+ *
+ * @author adriba
+ */
+public class TestMysqlJDBC {
+    
+    public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3307/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+         
+    try {
+            //puede ser que sea requerido lo siguiente
+            //Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection(url,"root", "piqui4");
+            Statement instruccion = conexion.createStatement();
+            String sql="SELECT Id_persona, Nombre, Apellidos, Edad FROM persona";
+            ResultSet resultado = instruccion.executeQuery(sql);
+             while (resultado.next()){
+                System.out.println("Id Persona: " + resultado.getInt("Id_persona"));
+                System.out.println("Nombre: " + resultado.getString("Nombre"));
+                System.out.println("Apellidos " + resultado.getString("Apellidos"));
+                System.out.println("Edad: " + resultado.getInt("Edad"));
+
+            }
+             resultado.close();
+             instruccion.close();
+             conexion.close();
+        } catch (SQLException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
+    
+         
+}
