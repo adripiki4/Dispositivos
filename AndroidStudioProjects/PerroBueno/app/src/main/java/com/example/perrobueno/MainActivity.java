56ee0851b5ed2316f394ed18243600a68848daeb
adripiki4 <adriba@alu.iesserpis.org>
2020-11-20 11:26:28 +0100

    acabado ejercicio del perrito

--- AndroidStudioProjects/PerroBueno/app/src/main/java/com/example/perrobueno/MainActivity.java
+++ AndroidStudioProjects/PerroBueno/app/src/main/java/com/example/perrobueno/MainActivity.java
@@ -3,33 +3,71 @@ package com.example.perrobueno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
 
-    private Button show_pet;
-    public final static String nombreperro = "TU PERRITO DE CONFIANZA";
+    public static int COD_RESPUESTA=0;
+    TextView opinion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        show_pet = findViewById(R.id.show_pet_button);
+        final Button show_pet = findViewById(R.id.show_pet_button);
+        final TextView page = findViewById(R.id.pageweb);
+        opinion = (TextView)findViewById(R.id.opinion_text);
 
         show_pet.setOnClickListener(new View.OnClickListener() {
+            String nombreperro = "TU PERRITO DE CONFIANZA";
+
             @Override
             public void onClick(View v) {
+
                 Intent myintent = new Intent(MainActivity.this, Pantalla2.class);
                 Bundle mybundle = new Bundle();
                 mybundle.putString("nombre",nombreperro);
                 myintent.putExtras(mybundle);
-                startActivity(myintent);
+                startActivityForResult(myintent,COD_RESPUESTA);
             }
         });
 
+        page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://hermosaprogramacion.blogspot.com");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+                // Verificar si hay aplicaciones disponibles
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent, 0);
+                boolean isIntentSafe = activities.size() > 0;
+
+                // Si hay, entonces ejecutamos la actividad
+                if (isIntentSafe) {
+                    startActivity(webIntent);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int cod_resp, int cod_result,Intent intent) {
+        super.onActivityResult(cod_resp, cod_result, intent);
+        if (cod_result == RESULT_OK) {
+            Bundle otroBundle = intent.getExtras();
+            opinion.setText(otroBundle.getString("respuesta"));
+        }
     }
 
 }
--- AndroidStudioProjects/PerroBueno/app/src/main/java/com/example/perrobueno/Pantalla2.java
+++ AndroidStudioProjects/PerroBueno/app/src/main/java/com/example/perrobueno/Pantalla2.java
@@ -2,20 +2,70 @@ package com.example.perrobueno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 
 public class Pantalla2 extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
+
         final TextView ponnombre = findViewById(R.id.txtimage);
+        final RadioGroup devuelve = findViewById(R.id.opiniones_group);
+        final Button envviar = findViewById(R.id.send_button);
+        final String[] respuesta = {""};
+
 
         Bundle recoge = getIntent().getExtras();
         ponnombre.setText(recoge.getString("nombre"));
+
+        devuelve.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+
+                switch (devuelve.getCheckedRadioButtonId()) {
+                    case R.id.radio_hermoso:
+                        respuesta[0] = "Hermoso";
+                        break;
+                    case R.id.radio_lindo:
+                        respuesta[0] = "Nindo";
+                        break;
+                    case R.id.radio_normal:
+                        respuesta[0] = "Normal";
+                        break;
+                    case R.id.radio_horroroso:
+                        respuesta[0] = "Horroroso";
+                    default:
+                        break;
+                }
+
+            }
+        });
+
+        envviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent databack = new Intent();
+                Bundle sending = new Bundle();
+                sending.putString("respuesta", respuesta[0]);
+                databack.putExtras(sending);
+                setResult(RESULT_OK,databack);
+                finish();
+
+            }
+        });
+
+
     }
 }
