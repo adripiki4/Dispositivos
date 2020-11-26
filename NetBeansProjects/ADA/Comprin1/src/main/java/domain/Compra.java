09cdce432cc8b7a6013dd989508644e05bf3b522
adripiki4 <adripiki4@gmail.com>
2020-11-26 13:58:50 +0100

    Haciendo SuperComprin

--- NetBeansProjects/ADA/Comprin1/src/main/java/domain/Compra.java
+++ NetBeansProjects/ADA/Comprin1/src/main/java/domain/Compra.java
@@ -5,10 +5,76 @@
  */
 package domain;
 
+import java.sql.Date;
+
 /**
  *
  * @author adrip
  */
 public class Compra {
+
+    private int Id_compra;
+    private Date FechaCompra;
+    private int Id_wallet;
+    private int Id_producto;
+
+    public Compra() {
+
+    }
+
+    public Compra(int Id_compra, Date FechaCompra, int Id_wallet, int Id_producto) {
+        this.Id_compra = Id_compra;
+        this.FechaCompra = FechaCompra;
+        this.Id_wallet = Id_wallet;
+        this.Id_producto = Id_producto;
+    }
+
+    public Compra(Date FechaCompra, int Id_wallet, int Id_producto) {
+        this.FechaCompra = FechaCompra;
+        this.Id_wallet = Id_wallet;
+        this.Id_producto = Id_producto;
+    }
+
+    public Compra(int Id_compra) {
+        this.Id_compra = Id_compra;
+    }
+
+    public int getId_compra() {
+        return Id_compra;
+    }
+
+    public void setId_compra(int Id_compra) {
+        this.Id_compra = Id_compra;
+    }
+
+    public Date getFechaCompra() {
+        return FechaCompra;
+    }
+
+    public void setFechaCompra(Date FechaCompra) {
+        this.FechaCompra = FechaCompra;
+    }
+
+    public int getId_wallet() {
+        return Id_wallet;
+    }
+
+    public void setId_wallet(int Id_wallet) {
+        this.Id_wallet = Id_wallet;
+    }
+
+    public int getId_producto() {
+        return Id_producto;
+    }
+
+    public void setId_producto(int Id_producto) {
+        this.Id_producto = Id_producto;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra{" + "Id_compra=" + Id_compra + ", FechaCompra=" + FechaCompra + ", Id_wallet=" + Id_wallet + ", Id_producto=" + Id_producto + '}';
+    }
+    
     
 }
--- NetBeansProjects/ADA/Comprin1/src/main/java/domain/Producto.java
+++ NetBeansProjects/ADA/Comprin1/src/main/java/domain/Producto.java
@@ -10,5 +10,64 @@ package domain;
  * @author adrip
  */
 public class Producto {
-    
+
+    private int Id_producto;
+    private String NombreProducto;
+    private int PrecioProducto;
+    private int PuntosProducto;
+
+    public Producto() {
+
+    }
+
+    public Producto(int Id_producto, String NombreProducto, int PrecioProducto, int PuntosProducto) {
+        this.Id_producto = Id_producto;
+        this.NombreProducto = NombreProducto;
+        this.PrecioProducto = PrecioProducto;
+        this.PuntosProducto = PuntosProducto;
+    }
+
+    public Producto(String NombreProducto, int PrecioProducto, int PuntosProducto) {
+        this.NombreProducto = NombreProducto;
+        this.PrecioProducto = PrecioProducto;
+        this.PuntosProducto = PuntosProducto;
+    }
+
+    public int getId_producto() {
+        return Id_producto;
+    }
+
+    public void setId_producto(int Id_producto) {
+        this.Id_producto = Id_producto;
+    }
+
+    public String getNombreProducto() {
+        return NombreProducto;
+    }
+
+    public void setNombreProducto(String NombreProducto) {
+        this.NombreProducto = NombreProducto;
+    }
+
+    public int getPrecioProducto() {
+        return PrecioProducto;
+    }
+
+    public void setPrecioProducto(int PrecioProducto) {
+        this.PrecioProducto = PrecioProducto;
+    }
+
+    public int getPuntosProducto() {
+        return PuntosProducto;
+    }
+
+    public void setPuntosProducto(int PuntosProducto) {
+        this.PuntosProducto = PuntosProducto;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "Id_producto=" + Id_producto + ", NombreProducto=" + NombreProducto + ", PrecioProducto=" + PrecioProducto + ", PuntosProducto=" + PuntosProducto + '}';
+    }
+
 }
