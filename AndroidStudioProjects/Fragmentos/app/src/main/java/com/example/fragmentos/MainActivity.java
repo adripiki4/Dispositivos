fe308779a9f09495138f5dc8125aceb8edd54815
adripiki4 <adriba@alu.iesserpis.org>
2020-12-07 10:43:23 +0100

    subiendo

--- AndroidStudioProjects/Fragmentos/app/src/main/java/com/example/fragmentos/MainActivity.java
+++ AndroidStudioProjects/Fragmentos/app/src/main/java/com/example/fragmentos/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.fragmentos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
+import static com.example.fragmentos.R.id.relative2;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        CountFragment fragment = new CountFragment();
+        transaction.add(relative2, fragment);
     }
 }
