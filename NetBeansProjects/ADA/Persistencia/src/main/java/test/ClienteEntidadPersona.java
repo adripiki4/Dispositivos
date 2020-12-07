f63d5784be5ed781a4464e4ca876c68f441802bd
adripiki4 <adriba@alu.iesserpis.org>
2020-12-07 12:02:59 +0100

    subiendo persistencia

--- NetBeansProjects/ADA/Persistencia/src/main/java/test/ClienteEntidadPersona.java
+++ NetBeansProjects/ADA/Persistencia/src/main/java/test/ClienteEntidadPersona.java
@@ -15,13 +15,29 @@ public class ClienteEntidadPersona {
 
         //iniciamos la transaccion
         tx.begin();
-        Persona persona1 = new Persona("Pedro", "Linares", "pedro@@", 45);
-        log.debug("objeto a persistir " + persona1);
-        //persistimos el objeto
-        em.persist(persona1);
-        //terminamos transaccion
+//        Persona persona1 = new Persona("Pedro", "Linares", "pedro@@", 45);
+//        log.debug("objeto a persistir " + persona1);
+//        //persistimos el objeto
+//        em.persist(persona1);
+//        //terminamos transaccion
+//        tx.commit();
+//        log.debug("Objeto persistido" + persona1);
+//        em.clear();
+
+        // HACER UNA BUSQUEDA
+        Persona persona2 = new Persona();
+        persona2 = em.find(Persona.class, 14);
+        System.out.println("La persona encontrada es " + persona2);
+
+        //ACTUALIZAR UN REGISTRO
+        persona2.setNombre("Juancho");
+        em.merge(persona2);
+
+        //Borrar un registro
+        Persona persona3 = new Persona();
+        persona3 = em.find(Persona.class, 12);
+        em.remove(persona3);
         tx.commit();
-        log.debug("Objeto persistido" + persona1);
-        em.clear();
+
     }
 }
