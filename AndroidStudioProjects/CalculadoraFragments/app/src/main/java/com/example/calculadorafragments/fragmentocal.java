7f7a5535e24316509220256b0e2e28d06b859347
adripiki4 <adriba@alu.iesserpis.org>
2021-01-07 23:06:50 +0100

    calculadora fragmentos acabada

--- AndroidStudioProjects/CalculadoraFragments/app/src/main/java/com/example/calculadorafragments/fragmentocal.java
+++ AndroidStudioProjects/CalculadoraFragments/app/src/main/java/com/example/calculadorafragments/fragmentocal.java
@@ -1,48 +1,88 @@
-package com.example.calculadoralayout;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.calculadorafragments;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private int primero = 0;
-    private int segundo = 0;
+public class fragmentocal  extends Fragment{
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
     private double resultado;
     private String operador,reserva,mostrar;
     private boolean punto = false;
     private boolean entra = true;
 
+    public fragmentocal() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment fragmentocal.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static fragmentocal newInstance(String param1, String param2) {
+        fragmentocal fragment = new fragmentocal();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view  = inflater.inflate(R.layout.fragmentocal, container, false);
 
-        final TextView Resultado = findViewById(R.id.txtprincipal);
+        final TextView Resultado = view.findViewById(R.id.txtprincipal);
 
-        final Button btncero = findViewById(R.id.numero_cero);
-        final Button btnuno = findViewById(R.id.numero_1);
-        final Button btndos = findViewById(R.id.numero_2);
-        final Button btntres = findViewById(R.id.numero_3);
-        final Button btncuatro = findViewById(R.id.numero_4);
-        final Button btncinco = findViewById(R.id.numero_5);
-        final Button btnseis = findViewById(R.id.numero_6);
-        final Button btnsiete = findViewById(R.id.numero_7);
-        final Button btnocho = findViewById(R.id.numero_8);
-        final Button btnnueve = findViewById(R.id.numero_9);
+        final Button btncero = view.findViewById(R.id.numero_cero);
+        final Button btnuno = view.findViewById(R.id.numero_1);
+        final Button btndos = view.findViewById(R.id.numero_2);
+        final Button btntres = view.findViewById(R.id.numero_3);
+        final Button btncuatro = view.findViewById(R.id.numero_4);
+        final Button btncinco = view.findViewById(R.id.numero_5);
+        final Button btnseis = view.findViewById(R.id.numero_6);
+        final Button btnsiete = view.findViewById(R.id.numero_7);
+        final Button btnocho = view.findViewById(R.id.numero_8);
+        final Button btnnueve = view.findViewById(R.id.numero_9);
 
-        final Button btnpunto = findViewById(R.id.punto);
-        final Button btnigual = findViewById(R.id.signo_igual);
-        final Button btndivide = findViewById(R.id.signo_divide);
-        final Button btnpor = findViewById(R.id.signo_por);
-        final Button btnsuma = findViewById(R.id.signo_mas);
-        final Button btnmenos = findViewById(R.id.signo_menos);
-        final Button btnborra = findViewById(R.id.signo_borra);
-        final Button btndelete = findViewById(R.id.signo_borrauno);
-
+        final Button btnpunto = view.findViewById(R.id.punto);
+        final Button btnigual = view.findViewById(R.id.signo_igual);
+        final Button btndivide = view.findViewById(R.id.signo_divide);
+        final Button btnpor = view.findViewById(R.id.signo_por);
+        final Button btnsuma = view.findViewById(R.id.signo_mas);
+        final Button btnmenos = view.findViewById(R.id.signo_menos);
+        final Button btnborra = view.findViewById(R.id.signo_borra);
+        final Button btndelete = view.findViewById(R.id.signo_borrauno);
 
 
         //Funcionalidad de los botones
@@ -247,7 +287,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
+        return view;
     }
+
+
 }
