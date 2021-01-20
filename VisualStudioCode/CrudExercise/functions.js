60d150c2480dbbde96eeff837c530837435f0a08
adripiki4 <adriba@alu.iesserpis.org>
2021-01-20 13:39:06 +0100

    subiendo

--- VisualStudioCode/CrudExercise/functions.js
+++ VisualStudioCode/CrudExercise/functions.js
@@ -17,12 +17,15 @@ let customers = new Array()
 // with the data of a client
 customers = JSON.parse(file);
 
+let mostrarbtn = false;
 let position = 0
 let mostrarClienete = ()=>{
     document.getElementById('dni').value = customers[position].dni
     document.getElementById("nombre").value = customers[position].Name
     document.getElementById("telefono").value = customers[position].telephone
 }
+
+mostrarClienete();
 
 btnprimero.addEventListener('click', ()=>{
     position= 0;
@@ -39,7 +42,11 @@ btnatras.addEventListener('click',()=>{
 })
 
 btnadelante.addEventListener('click', ()=>{
-    position= position +1;
+    if(position==(customers.length-1)){
+        position = customers.length -1;
+    }else{
+        position= position +1;
+    }
     mostrarClienete();
 })
 
@@ -49,9 +56,71 @@ btnultimo.addEventListener('click', ()=>{
 })
  
 btnborrar.addEventListener('click', ()=>{
-    position= 0;
+    customers.splice(position,1);
+
+    //Si borra la ultima posicion, el array tendra un valor menos, por la tanto la posicion anterior ya no será .length-1 si no .length
+    if(position==(customers.length)){
+        position = position -1;
+    }else{
+        position= position = 0;
+    }
+
+    if(customers.length==0){
+        document.getElementById('dni').value = "";
+        document.getElementById('nombre').value = "";
+        document.getElementById('telefono').value = "";
+    }
     mostrarClienete();
+    //actulizarfichero();
 })
+
+btnactualizar.addEventListener('click', ()=>{
+    //obtiene los datos de la pantalla
+    customers[position].dni = document.getElementById('dni').value;
+    customers[position].Name = document.getElementById('nombre').value
+    customers[position].telephone = document.getElementById('telefono').value
+    actulizarfichero();
+})
+
+let actulizarfichero = () =>{
+    fs.writeFileSync('./customers.json',JSON.stringify(customers));
+}
+
+btninsertar.addEventListener('click',()=>{
+    ocultarbtns();
+    let nuevo = {}
+    if(mostrarbtn== false){
+        document.getElementById('dni').value = "";
+        document.getElementById('nombre').value = "";
+        document.getElementById('telefono').value = "";
+        mostrarbtn = true
+        //Cambia la clase de btn
+        btninsertar.classList.remove("btn-primary")
+        btninsertar.classList.add("btn-negative")
+    }else{
+        nuevo={
+            dni: document.getElementById('dni').value,
+            Name: document.getElementById('nombre').value,
+            telephone: document.getElementById('telefono').value
+        }
+    }
+ 
+
+})
+
+let ocultarbtns = () =>{
+
+    if(mostrarbtn==false){
+        btnprimero.disable.true;
+        btnatras.disable.true;
+        btnadelante.disable.true;
+        btnultimo.disable.true;
+        btnborrar.disable.true;
+        btnactualizar.disable.true;
+
+    
+    }
+}
 
 
 
