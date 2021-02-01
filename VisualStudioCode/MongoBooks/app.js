4b1c8c89aebce8c5ff4aa7841a05a43a2a1f4413
adripiki4 <adriba@alu.iesserpis.org>
2021-02-01 13:37:40 +0100

    mongobooks

---
+++ VisualStudioCode/MongoBooks/app.js
@@ -0,0 +1,62 @@
+const { error } = require("console");
+const fs = require("fs"); 
+//Read
+
+// Read books.json file 
+let fichero = fs.readFileSync('./books.json',);
+let books = JSON.parse(fichero);
+
+
+// we load moongose
+const mongoose=require('mongoose');
+// we set the default promise engine to javascript
+// this is necessary because mongo supports different types of promises
+mongoose.Promise=global.Promise;
+// connect with the contacts DB
+mongoose.connect('mongodb://localhost:27017/books', {
+useNewUrlParser:true,
+useUnifiedTopology:true
+});
+
+//scheme
+//esquem
+let BookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true,
+        
+    },
+    img: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    }
+});
+
+//model
+let book = mongoose.model('books', BookSchema);
+let p1
+books.forEach(element => {
+    let Book = new book();
+        Book.title= element.title;
+        Book.author = element.author;
+        Book.img = element.img;
+        p1 = Book.save().then(resultado =>{
+            console.log("Book id added: ", resultado);
+        }).catch(error=>{
+            console.log(error);
+        })
+
+});
+
+
+
+
